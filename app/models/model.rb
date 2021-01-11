b18dd0c721b521e7fd36cae5569cc68025cdf9c8
Alex Marz <alexhmarz@gmail.com>
2021-01-11 00:05:05 -0500

    commit tree 5f899c4540965f633989913d1e89f1b18e7bbfae

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,20 @@
-class Model
+class Dog
+    attr_accessor :name, :breed, :age
+    @@dog = []
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+
+        @@dog << self
+
+    end
+
+    def self.all 
+        @@dog
+    end
+    
+
 
 end
